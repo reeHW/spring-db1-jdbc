96ae73f140c238285c52cc07ce07f05971b56785
reeHW <hjkh0107@gmail.com>
2023-05-11 19:29:20 +0900

    DataSource 적용 - HikariDataSource 사용

--- src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,5 +1,6 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
@@ -21,8 +22,13 @@ class MemberRepositoryV1Test {
     @BeforeEach
     void beforeEach(){
         // 기본 DriverManager - 항상 새로운 커넥션을 획득
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        //DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
         repository = new MemberRepositoryV1(dataSource);
     }
 
@@ -46,6 +52,11 @@ class MemberRepositoryV1Test {
         //delete
         repository.delete(member.getMemberId());
 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
